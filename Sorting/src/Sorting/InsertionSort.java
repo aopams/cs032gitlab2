8374a43a34afb4ef3da5bada17083cb0c06069a4
Harsha Yeddanapudy <hyeddana@cs.brown.edu>
2015-04-11 14:43:44 -0400

    insert sort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -9,7 +9,16 @@ public class InsertionSort {
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
 		//TODO: S2 implements insertion sort
-		
-		return null;
+	  int n = array.length;
+    for (int j = 1; j < n; j++) {
+        int key = array[j];
+        int i = j-1;
+        while ( (i > -1) && ( array [i] > key ) ) {
+            array [i+1] = array [i];
+            i--;
+        }
+        array[i+1] = key;
+    }
+		return array;
 	}
 }
