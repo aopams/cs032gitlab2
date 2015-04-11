844d7269282a7237c4f9b306e221d0bb3fd1e055
Andrew Osgood <andrew_osgood@brown.edu>
2015-04-11 14:44:56 -0400

    finished bubble

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -6,9 +6,24 @@ public class BubbleSort {
 	 * @param unsortedArray		an unsorted array of integers
 	 * @return					the input array, sorted least to greatest
 	 */
-	public static int[] bubbleSort(int[] unsortedArray) {
+	public static int[] bubbleSort(int[] array) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+	  boolean swapped = true;
+    int j = 0;
+    int tmp;
+    while (swapped) {
+          swapped = false;
+          j++;
+          for (int i = 0; i < array.length - j; i++) {                                       
+                if (array[i] > array[i + 1]) {                          
+                      tmp = array[i];
+                      array[i] = array[i + 1];
+                      array[i + 1] = tmp;
+                      swapped = true;
+                }
+          }                
+    }
+
+    return array;
 	}
 }
